743bbc3f260d329a2bc2fb08d3acce2eaa616ed2
Caner Kuru <86489794+kurucaner@users.noreply.github.com>
2025-11-30 19:58:29 -0500

    header added to ampex

--- apps/worker/src/scrappers/scrape-ampex.ts
+++ apps/worker/src/scrappers/scrape-ampex.ts
@@ -16,7 +16,19 @@ export async function scrapeAMPEX(
 
   try {
     console.info(`🔍 Scraping AMPEX - ${productConfig.name}...`);
-    const response = await fetch(url);
+    const response = await fetch(url, {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        Referer: baseUrl, // The base URL of the site
+        Connection: 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+      },
+    });
     if (!response.ok) {
       throw new Error(`HTTP ${response.status}: ${response.statusText}`);
     }
