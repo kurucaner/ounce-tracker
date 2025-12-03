4ef0a6ca3617b2f42c824ed036be2884613e00e0
Caner Kuru <86489794+kurucaner@users.noreply.github.com>
2025-12-03 14:43:13 -0500

    favicons added

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -29,23 +29,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link
-        rel="android-chrome-192x192"
-        type="image/png"
-        sizes="192x192"
-        href="/android-chrome-192x192.png"
-      />
-      <link
-        rel="android-chrome-512x512"
-        type="image/png"
-        sizes="512x512"
-        href="/android-chrome-512x512.png"
-      />
+      <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
       <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="favicon-96x96" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-      <link rel="favicon-32x32" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="favicon-16x16" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="favicon" type="image/x-icon" sizes="48x48" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="icon" type="image/x-icon" sizes="48x48" href="/favicon.ico" />
       <link rel="manifest" href="/site.webmanifest" />
       <body className={inter.className}>
         <Toaster />
